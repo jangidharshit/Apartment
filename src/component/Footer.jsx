62439f62818fe44f33ec96bf1eaf59d3a52163ad
zangidharshit <your-email@gmail.com>
2026-04-21 14:24:11 +0530

    fix footer links final final

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -44,9 +44,9 @@ const Footer = () => {
         <div>
           <h4>Legal</h4>
           <ul className="footer-links">
-            <li><a href="#">Privacy Policy</a></li>
-            <li><a href="#">Terms of Service</a></li>
-            <li><a href="#">Cookie Policy</a></li>
+            <li><a href="/privacy">Privacy Policy</a></li>
+            <li><a href="/terms">Terms of Service</a></li>
+            <li><a href="/cookies">Cookie Policy</a></li>
           </ul>
         </div>
 
